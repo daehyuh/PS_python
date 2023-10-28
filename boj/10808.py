1703e244cfff0dff3c931b5ccd1b02c54173ae75
대현 <rkdeown10@naver.com>
2023-10-28 22:16:30 +0900

    10월28일

--- boj/10808.py
+++ boj/10808.py
@@ -1,7 +1,7 @@
-n = input()
-arr = [0] * 26
-for i in n:
-    arr[int(ord(i))-97] += 1
+a = int(input())
 
-for i in arr:
-    print(i, end=' ')
+arr = []
+
+for i in range(a):
+    a = input()
+    print(f'{i+1}. {a}')
